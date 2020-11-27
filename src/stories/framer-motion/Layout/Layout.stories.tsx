0ec43249fb7953a7d0625485497320e0fea682e7
hazem osama ibrahim <hazem3500@gmail.com>
2020-11-27 16:27:00 +0200

    feat(framer-motion): add grid layout story

--- src/stories/framer-motion/Layout/Layout.stories.tsx
+++ src/stories/framer-motion/Layout/Layout.stories.tsx
@@ -6,11 +6,12 @@ import {
     HStack,
     Heading,
     Flex,
+    Grid,
 } from '@chakra-ui/core';
 import React, { useState } from 'react';
 import MotionBox from '../../../components/MotionBox/MotionBox';
 import StyledMotionBox from '../../../components/StyledMotionBox/StyledMotionBox';
-import { useAnimation } from 'framer-motion';
+import { AnimatePresence, AnimateSharedLayout } from 'framer-motion';
 
 export default {
     title: 'framer-motion/layout',
@@ -57,6 +58,52 @@ export const flex = () => {
                         {justifyValue}
                     </Button>
                 ))}
+            </HStack>
+        </Stack>
+    );
+};
+
+export const grid = () => {
+    const [numOfBoxes, setNumOfBoxes] = useState(10);
+
+    const item = {
+        visible: { opacity: 1, scale: 1 },
+        hidden: { opacity: 0, scale: 0.5 },
+    };
+
+    return (
+        <Stack spacing={16} w="full">
+            <Grid
+                justifyItems="center"
+                templateColumns="repeat(5, 1fr)"
+                gap={6}
+                w="full"
+            >
+                <AnimatePresence>
+                    {new Array(numOfBoxes).fill(null).map((_, i) => (
+                        <StyledMotionBox
+                            key={i}
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                            variants={item}
+                            layout
+                        />
+                    ))}
+                </AnimatePresence>
+            </Grid>
+            <HStack>
+                <Heading size="md">Boxes({numOfBoxes}): </Heading>
+                <Button
+                    onClick={() =>
+                        numOfBoxes > 1 && setNumOfBoxes(numOfBoxes - 1)
+                    }
+                >
+                    Remove -
+                </Button>
+                <Button onClick={() => setNumOfBoxes(numOfBoxes + 1)}>
+                    Add +
+                </Button>
             </HStack>
         </Stack>
     );
